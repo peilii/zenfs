b5674a2c6a0287bb007e1ca354daac13f287ea79
Soumendu Sekhar Satapathy <49257870+soumendus@users.noreply.github.com>
2021-06-27 11:59:08 -0400

    Update fs_zenfs.h
    friend class ZenFSGCWorker;

--- fs/fs_zenfs.h
+++ fs/fs_zenfs.h
@@ -99,6 +99,8 @@ class ZenMetaLog {
 };
 
 class ZenFS : public FileSystemWrapper {
+  
+  friend class ZenFSGCWorker;
   ZonedBlockDevice* zbd_;
   std::map<std::string, ZoneFile*> files_;
   std::mutex files_mtx_;
